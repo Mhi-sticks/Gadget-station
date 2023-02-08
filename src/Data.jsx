a8c466d077604e7c3933d6eb25f45911407d8f5a
Daniel <adeosundaniel33@gmail.com>
2023-02-09 00:27:44 +0100

    data component for iphone contenets

---
+++ src/Data.jsx
@@ -0,0 +1,74 @@
+const DATA = [
+  {
+    id: 0,
+    title: "Iphone 11",
+    price: 750,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone11.jpg",
+  },
+  {
+    id: 1,
+    title: "Iphone 11 Pro Max",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone11promax.jpg",
+  },
+  {
+    id: 2,
+    title: "Iphone 12 Mini",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone12mini.jpg",
+  },
+  {
+    id: 3,
+    title: "Iphone 12",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone12.jpg",
+  },
+  {
+    id: 4,
+    title: "Iphone 12 Pro",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone12pro.jpg",
+  },
+  {
+    id: 5,
+    title: "Iphone 12 Pro Max",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone12promax.jpg",
+  },
+  {
+    id: 6,
+    title: "Iphone 13",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone13.jpg",
+  },
+  {
+    id: 7,
+    title: "Iphone 13 Mini",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone13mini.jpg",
+  },
+  {
+    id: 8,
+    title: "Iphone 13 Pro Max",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone13promax.jpg",
+  },
+  {
+    id: 9,
+    title: "Iphone 11 Pro Max",
+    price: 800,
+    desc: "contrary to popular belief, lorem ipsum is not simply random text. It hasroots in a piece of classical latin literature from 45Bc, making it over 2000 years old, a latin professor named Richard Mclintock at hampden sydney college in virginia, looked up one of more obscure latin words consecuteur, from a lorem ipsum passage, and going through the cities of the world in classical literature, discovered the undoubtful source.",
+    img: "/assets/images/products/iphone11promax.jpg",
+  },
+];
+
+export default DATA;
