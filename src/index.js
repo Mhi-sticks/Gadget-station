ee1265164beb641cd27a1c82fae73c8aeb444d60
Daniel <adeosundaniel33@gmail.com>
2023-02-09 01:54:29 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "font-awesome/css/font-awesome.min.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Store from "./redux/Store";
 import { Provider } from "react-redux";
 
@@ -19,7 +18,4 @@ root.render(
   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
